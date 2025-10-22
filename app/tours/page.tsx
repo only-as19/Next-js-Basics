a9a711819767b9ed5eb63a00155bd3e2c10d5122
Asad <hellohello51981@gmail.com>
2025-10-22 22:28:05 +0500

    refactor fetch process

--- app/tours/page.tsx
+++ app/tours/page.tsx
@@ -7,13 +7,17 @@ type Tours = {
     price: string;
 }
 
+const customFetch = async () => {
+    await new Promise((resolve)=> setTimeout(resolve,3000))
+    const response = await fetch(url)
+    const data: Tours[] = await response.json()
+    return data
+}
+
 const url = 'https://www.course-api.com/react-tours-project';
 
 const ToursPage = async () => {
-
-    const response = await fetch(url)
-    const data: Tours[] = await response.json()
-    console.log(data)
+    const data = await customFetch()
   return (
     <div>
       <h1 className="text-5xl font bold">ToursPage</h1>
