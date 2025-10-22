a8332580483ebe8469b2a4fb102497b7f486c25f
Asad <hellohello51981@gmail.com>
2025-10-22 22:11:14 +0500

    make counter page the server component

--- app/counter/page.tsx
+++ app/counter/page.tsx
@@ -1,19 +1,11 @@
-'use client'
+import Counter from '@/components/Counter';
 
-import { useState } from "react";
-
-function Counter() {
-  const [count, setCount] = useState(0);
+function CounterPage() {
   return (
-    <div className='flex flex-col items-center w-[100px]'>
-      <p className='text-5xl font-bold'>{count}</p>
-      <button
-        onClick={() => setCount(count + 1)}
-        className='bg-blue-500 rounded-md text-white px-4 py-2 mt-4'
-      >
-        Increment
-      </button>
-    </div>
+    <section>
+      <h1 className='text-6xl mb-16'>Page Content</h1>
+      <Counter />
+    </section>
   );
 }
-export default Counter;
+export default CounterPage;
