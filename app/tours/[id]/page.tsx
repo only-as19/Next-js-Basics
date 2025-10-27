7ca80a9cb833f2b72c0cc2772f9e3ac2473db84d
Asad <hellohello51981@gmail.com>
2025-10-27 22:18:04 +0500

    feat: Import local image"

--- app/tours/[id]/page.tsx
+++ app/tours/[id]/page.tsx
@@ -1,13 +1,26 @@
-const page = async ({params}: {params:Promise<{id:string}>}) => {
-    const {id} = await params
+import pic from "@/images/pic.jpeg";
+import Image from "next/image";
+const page = async ({ params }: { params: Promise<{ id: string }> }) => {
+  const { id } = await params;
 
   return (
     <div>
-      <h1 className='text-4xl'>ID : {id}</h1>
-      
+      <h1 className="text-4xl">ID : {id}</h1>
+      <section className="flex gap-x-4 mt-4">
+        <div>
+          <Image
+            src={pic}
+            alt="maps"
+            width={192}
+            priority
+            height={192}
+            className="w-48 h-48 object-cover rounded"
+          />
+          <h2>local image</h2>
+        </div>
+      </section>
     </div>
   );
-  
-}
+};
 
-export default page
+export default page;
